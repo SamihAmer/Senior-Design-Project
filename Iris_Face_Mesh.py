f0becd15e8f657ea92de79079be85bbbcdd5c755
surya-vishnubhatt <spvishnubhatt@ucdavis.edu>
2022-04-29 02:58:48 -0700

    MR. IS CLEANED

--- Iris_Face_Mesh.py
+++ Iris_Face_Mesh.py
@@ -9,24 +9,15 @@ import pandas as pd
 import bottleneck as bn
 import time
 
-
-#mp.calculators.util.landmarks_smoothing_calculator_pb2
-
 mp_drawing = mp.solutions.drawing_utils       #using drawing utils mediapipe solution to draw
 mp_face_mesh = mp.solutions.face_mesh         #using face_mesh mediapipe solution to apply face mesh
 mp_drawing_syles = mp.solutions.drawing_styles  # this just lets us get fancy with how we can draw what we want
-
-
 mp_face_mesh.landmarks_refinement_calculator_pb2
 mp_face_mesh.landmarks_smoothing_calculator_pb2
-
-
 #specifications of face mesh drawing ( WE DONT WANT TO DRAW THE WHOLE FACE MESH SO THIS IS POINTLESS)
 drawing_spec = mp_drawing.DrawingSpec(color=(0, 0, 255),thickness= 1,
                                       circle_radius = 1)
-
 video = cv2.VideoCapture(0)      # using opencv to access camera with VideoCapture function, upload .mp4 files
-
 # video.set(3, 640)    # width
 # video.set(4, 1000)    # height
 # video.set(10, 100)   # brightness
@@ -38,87 +29,50 @@ rightmvmnt_R = []
 def rollavg_bottlneck(a,n): #moving average: https://www.delftstack.com/howto/python/moving-average-python/
     return bn.move_mean(a, window=n,min_count = None)
 start = time.time()
-
-
+# NOW WE ARE ENTERING THE FACEMESH
 with mp_face_mesh.FaceMesh(min_detection_confidence = 0.9,       #initializing detection confidences of face_mesh
                            min_tracking_confidence= 0.9) as face_mesh:
-
-
     # while loop to capture video, draw facemesh landmarks
     while True:
         ret, image = video.read()
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-
-
         results = face_mesh.process(image)
-        #print(results)
-
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-
         width = int(video.get(3))
         height = int(video.get(4))
-
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         lower_blue = np.array([90, 50, 50])
         upper_blue = np.array([130, 255, 255])
-
         mask = cv2.inRange(hsv, lower_blue, upper_blue)
-
         result = cv2.bitwise_and(image, image, mask=mask)
-
         # cv2.imshow('frame', result)
         # cv2.imshow('mask', mask)
-
-
-
-
-
-
-
-
-
-        if results.multi_face_landmarks:                                 #drawing landmarks
+        if results.multi_face_landmarks: #drawing landmarks
             for face_landmarks in results.multi_face_landmarks:
                 mp_drawing.draw_landmarks(image = image ,
                                             landmark_list = face_landmarks,
                                             connections = mp_face_mesh.FACEMESH_IRISES,  #specifying the iris connections
                                             landmark_drawing_spec = None,
                                             connection_drawing_spec = mp_drawing_syles.get_default_face_mesh_iris_connections_style())
-
                 mp_drawing.draw_landmarks(image=image,
                                           landmark_list=face_landmarks,
                                           connections=mp_face_mesh.FACEMESH_RIGHT_EYE,  # specifying the iris connections
                                           landmark_drawing_spec=None,
                                           connection_drawing_spec=mp_drawing_syles.get_default_face_mesh_contours_style())
-
                 mp_drawing.draw_landmarks(image=image,
                                           landmark_list=face_landmarks,
                                           connections=mp_face_mesh.FACEMESH_LEFT_EYE,
                                           # specifying the iris connections
                                           landmark_drawing_spec=None,
                                           connection_drawing_spec=mp_drawing_syles.get_default_face_mesh_contours_style())
-
-
                 h, w, c = image.shape
                 cx_min = w
                 cy_min = h
                 cx_max = cy_max = 0
-
-                # print(h, w, c)
-
-
-
-
-                # eye_landmarks = OrderedDict([("right eye"))
-
-
                 for id, lm in enumerate(face_landmarks.landmark):
-
                     x,y = int(lm.x*w), int(lm.y*h)
-                    # cv2.putText(image, str(id), (x,y), cv2.FONT_HERSHEY_PLAIN, 0.4, (0, 255, 0), 1 )
+                    #cv2.putText(image, str(id), (x,y), cv2.FONT_HERSHEY_PLAIN, 0.4, (0, 255, 0), 1 )
                     cx, cy = int(lm.x * w), int(lm.y * h)
                     if cx < cx_min:
                         cx_min = cx
@@ -129,12 +83,9 @@ with mp_face_mesh.FaceMesh(min_detection_confidence = 0.9,       #initializing d
                     if cy > cy_max:
                         cy_max = cy
                 cv2.rectangle(image, (cx_min, cy_min), (cx_max, cy_max), (255, 255, 0), 2)
-                #
-                # # print(cx_min, cy_min, cx_max, cy_max)
-                # print(id, lm)
-
-                #RIGHT IRIS
-                idx = 468 #4 IS NOSE   468
+                
+                #RIGHT PUPIL
+                idx = 468 # RIGHT PUPIL LANDMARK
                 # 133 left right eye
                 # 33 is right right eye
                 loc_x = int(face_landmarks.landmark[idx].x * image.shape[1])
@@ -154,13 +105,8 @@ with mp_face_mesh.FaceMesh(min_detection_confidence = 0.9,       #initializing d
                 # print("LEFT", rel_rlx)
                 # print("RIGHT", rel_rrx)
                 cv2.circle(image, (loc_x, loc_y), 2, (255, 255, 255), 2)
-                #time = np.linspace
 
-
-
-
-
-                #LEFT IRIS   473
+                #LEFT PUPIL   
                 idx2 = 473
                 # left right eye 362
                 # left left eye 263
@@ -181,14 +127,12 @@ with mp_face_mesh.FaceMesh(min_detection_confidence = 0.9,       #initializing d
                 # print("LEFT", rel_llx)
                 # print("RIGHT", rel_lrx)
                 cv2.circle(image, (loc_x2, loc_y2), 2, (255, 255, 255), 2)
-
-
-
-                            #display output image
-        #DATA NORMALIZATION
+                
+        #Display Output Image
         cv2.imshow("Face Mesh", image)
         k = cv2.waitKey(1)
         if k == ord('q'):
+
             #ELAPSED TIME CALCULATION
             end = time.time()
             elapsed = end-start
@@ -199,6 +143,7 @@ with mp_face_mesh.FaceMesh(min_detection_confidence = 0.9,       #initializing d
             for i in range(0, len(t)):
                 t[i] = elapsed*(i/(arrlen-1))
             #print(t)
+
             #NORMALIZATION IMPLEMENTATION
             norm1 = np.linalg.norm(rightmvmnt_L)
             normalizeR_L = rightmvmnt_L/norm1
@@ -208,10 +153,11 @@ with mp_face_mesh.FaceMesh(min_detection_confidence = 0.9,       #initializing d
             normalizeL_L = leftmvmnt_L/norm3 #normalize
             norm4 = np.linalg.norm(leftmvmnt_R)
             normalizeL_R = leftmvmnt_R/norm4
+            
             #RIGHT PUPIL MOTION CALCULATOR
             over_all_right = rollavg_bottlneck(normalizeR_L,5) + rollavg_bottlneck(normalizeR_R, 5)
             df1 = pd.DataFrame({"ELAPSED_SECONDS" : t, "RELATIVE_POSITION" : over_all_right})
-            df1.to_csv("left_pupil_data.csv", index=False)
+            df1.to_csv("right_pupil_data.csv", index=False)
             fig1 = plt.figure("Overall Right Eye Movement: Relative Position vs. Time Elapsed (s)")
             plt.plot(t, over_all_right, c = "black", lw = 2)
             plt.title("Right Pupil Movements")
@@ -221,20 +167,12 @@ with mp_face_mesh.FaceMesh(min_detection_confidence = 0.9,       #initializing d
             over_all_left = rollavg_bottlneck(normalizeL_L,5) + rollavg_bottlneck(normalizeL_R, 5)
             df = pd.DataFrame({"ELAPSED_SECONDS" : t, "RELATIVE_POSITION" : over_all_left})
             df.to_csv("left_pupil_data.csv", index=False)
-
-            fig3 = plt.figure("Overall Left Eye Movement: Relative Position vs. Time Elapsed (s)")
+            fig2 = plt.figure("Overall Left Eye Movement: Relative Position vs. Time Elapsed (s)")
             plt.plot(t, over_all_left, c = "black", lw = 2)
             plt.title("Left Pupil Movements")
             plt.savefig("LEFT_PUPIL_MOTION", format = "png")
             plt.show()
-            
 
             break
-
     video.release()
-    
     cv2.destroyAllWindows()
-
-
-
-    

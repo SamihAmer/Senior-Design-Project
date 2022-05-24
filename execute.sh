c7cf8249e1eaf76e6b3e13d39865773670b2e504
surya-vishnubhatt <86271349+surya-vishnubhatt@users.noreply.github.com>
2022-05-24 16:54:38 -0700

    USE MP4

--- execute.sh
+++ execute.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 cp ~/project/mediapipe_edits/face_mesh.py /Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/site-packages/mediapipe/python/solutions/
-count=`ls -1 *.mov 2>/dev/null | wc -l`
+count=`ls -1 *.mp4 2>/dev/null | wc -l`
 if [ $count == 0 ]
 then 
-echo PLEASE PUT .mov FILE INTO project DIRECTORY AND RE-RUN execute.sh
+echo PLEASE PUT .mp4 FILE INTO project DIRECTORY AND RE-RUN execute.sh
 exit 
 fi 
 if [ $count != 1 ]
 then 
-echo PLEASE ENSURE THAT ONLY ONE .mov FILE IS IN THE project DIRECTORY AND RE-RUN execute.sh
+echo PLEASE ENSURE THAT ONLY ONE .mp4 FILE IS IN THE project DIRECTORY AND RE-RUN execute.sh
 exit 
 fi 
-echo PLEASE ENTER PATIENT ID. THIS SHOULD MATCH THE TITLE OF THE .mov FILE
+echo PLEASE ENTER PATIENT ID. THIS SHOULD MATCH THE TITLE OF THE .mp4 FILE
 read varname
 echo THANK YOU
 pip install pipreqs

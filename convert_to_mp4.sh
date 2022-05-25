b794236f78888e599b7c5e354c1c556b456cb010
surya-vishnubhatt <86271349+surya-vishnubhatt@users.noreply.github.com>
2022-05-25 12:45:32 -0700

    Add files via upload

--- convert_to_mp4.sh
+++ convert_to_mp4.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-echo Please enter the name of the .mov file with the .mov included e.g. test.mov:
+echo Please enter the name of the .mov file without the .mov included:
 read varname
-echo Please enter the name of the output .mp4 file with the .mp4 included e.g. test.mp4:
-read varname1
-pip install ffmpeg
-ffmpeg -i $varname -qscale 0 $varname1
+brew install ffmpeg
+ffmpeg -i $varname.mov -qscale 0 $varname.mp4
